6c82ff4a198efaca12447b5d0157f27007a1631f
michaldzik97 <michaldzik97@gmail.com>
2018-05-07 21:47:42 +0200

    Update dateManager.js

--- dateManager.js
+++ dateManager.js
@@ -1,36 +1,3 @@
-// function mydate()
-// {
-// document.getElementById("dt").hidden=false;
-// document.getElementById("ndt").hidden=true;
-// }
-// function mydate1()
-// {
-//  d=new Date(document.getElementById("dt").value);
-// dt=d.getDate();
-// mn=d.getMonth();
-// mn++;
-// yy=d.getFullYear();
-// document.getElementById("ndt").value=dt+"/"+mn+"/"+yy
-// document.getElementById("ndt").hidden=false;
-// document.getElementById("dt").hidden=true;
-// }
-
-// function mydate(field1, field2)
-// {
-// document.getElementById(field1).hidden=false;
-// document.getElementById(field2).hidden=true;
-// }
-// function mydate1(field1, field2)
-// {
-// d=new Date(document.getElementById(field1).value);
-// dt=d.getDate();
-// mn=d.getMonth();
-// mn++;
-// yy=d.getFullYear();
-// document.getElementById(field2).value=dt+"/"+mn+"/"+yy
-// document.getElementById(field2).hidden=false;
-// document.getElementById(field1).hidden=true;
-// }
 
 function mydate(field1, field2)
 {
